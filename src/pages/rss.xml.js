dd2ebfb9769b04ab48718d4079fe386d0f4cdb26
孤久きりのなか <ZCX02172024@Gmail.com>
2026-08-12 15:23:22 +0800

    20260812 1523
    RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,13 +10,27 @@ export async function GET(context) {
     title: siteConfig.title,
     description: siteConfig.description,
     site: context.site,
+    stylesheet: '/styles/pretty-feed-v3.xsl',
     items: posts.map((post) => ({
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.pubDate,
       link: `/posts/${post.id}/`,
       categories: post.data.tags,
+      content: post.body,
+      author: profileConfig.email,
     })),
-    customData: `<language>${siteConfig.lang}</language>`,
+    customData: `
+      <language>${siteConfig.lang}</language>
+      <managingEditor>${profileConfig.name}</managingEditor>
+      <webMaster>${profileConfig.name} (${profileConfig.email})</webMaster>
+      <copyright>© ${new Date().getFullYear()} ${profileConfig.name}</copyright>
+      <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+      <image>
+        <url>${context.site}images/favicon.png</url>
+        <title>${siteConfig.title}</title>
+        <link>${context.site}</link>
+      </image>
+    `,
   });
 }
