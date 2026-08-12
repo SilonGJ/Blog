1b4961ceccb622b653108ed4d2861801995d520f
孤久きりのなか <ZCX02172024@Gmail.com>
2026-08-12 15:29:14 +0800

    20260812 1529
    RSS2

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,25 +1,35 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { createMarkdownProcessor } from '@astrojs/markdown-remark';
 import { siteConfig, profileConfig } from '../config';
 
 export async function GET(context) {
   const posts = (await getCollection('posts', ({ data }) => !data.draft))
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
+  const renderer = await createMarkdownProcessor();
+
+  const items = await Promise.all(
+    posts.map(async (post) => {
+      const { code: html } = await renderer.render(post.body);
+      return {
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.pubDate,
+        link: `/posts/${post.id}/`,
+        categories: post.data.tags,
+        content: html,
+        author: profileConfig.email,
+      };
+    })
+  );
+
   return rss({
     title: siteConfig.title,
     description: siteConfig.description,
     site: context.site,
     stylesheet: '/styles/pretty-feed-v3.xsl',
-    items: posts.map((post) => ({
-      title: post.data.title,
-      description: post.data.description,
-      pubDate: post.data.pubDate,
-      link: `/posts/${post.id}/`,
-      categories: post.data.tags,
-      content: post.body,
-      author: profileConfig.email,
-    })),
+    items,
     customData: `
       <language>${siteConfig.lang}</language>
       <managingEditor>${profileConfig.name}</managingEditor>
